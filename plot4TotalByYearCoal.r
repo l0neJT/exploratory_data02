d55c935c2ca05c1948a5ac77d8d27a63e2e1587c
l0neJT <loganjtravis@gmail.com>
2014-05-25 12:48:04 -0700

    Create plot4TotalByYearCoal.r Function
    Create plot4TotalByYearCoal.r function to plot total PM2.5 emissions by
    year for coal then save as PNG using ggplot2 package.

---
+++ plot4TotalByYearCoal.r
@@ -0,0 +1,41 @@
+##
+
+plot4TotalByYearCoal <- function(dat = readNEIdata(srcCols = c("SCC", "EI.Sector")),
+                                 file = "plot4TotalByYearCoal.png",
+                                 dir = ".",
+                                 years = NULL) {
+    # Load libraries
+    library(data.table, ggplot2)
+    
+    # Summarize data by year and EI sector
+    # Sets name for total PM2.5 tons
+    dat <- dat[, sum(Emissions), by = c("year", "EI.Sector")]
+    setnames(dat, c("year", "EI.Sector", "Total_PM2.5"))
+    
+    # Subset for 'years' if not NULL
+    if(!is.null(years)) dat <- dat[year %in% years, ]
+    
+    # Subset for coal
+    isCoal <- function(txt) {grepl("coal", txt, ignore.case = TRUE)}
+    dat <- dat[sapply(dat$EI.Sector, isCoal), ]
+    
+    # Define plot parameters
+    geom <- c("point", "smooth")
+    method <- "lm"
+    xlab <- ""
+    ylab <- "Tons PM2.5"
+    dpi <- 72
+    width <- 720 / dpi
+    height <- 480 / dpi
+    
+    # Create plot
+    p <- qplot(year, Total_PM2.5, data = dat, geom = geom, method = method,
+               col = EI.Sector, xlab = xlab, ylab = ylab)
+    
+    # Save plot
+    filename <- ifelse(dir == ".", file, paste(dir, file, sep = "/"))
+    ggsave(filename = filename, plot = p, width = width, height = height, dpi = dpi)
+    
+    # Return filename
+    filename
+}
