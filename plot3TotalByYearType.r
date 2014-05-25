31d5f989434ff14daa1cd03433c96bb66627d7b2
l0neJT <loganjtravis@gmail.com>
2014-05-25 13:30:46 -0700

    General Cleanup
    General cleanup to correct plots.

--- plot3TotalByYearType.r
+++ plot3TotalByYearType.r
@@ -41,9 +41,9 @@ plot3TotalByYearType <- function(dat = readNEIdata(),
     method <- "lm"
     xlab <- ""
     ylab <- "Tons PM2.5"
-    dpi <- 72
-    width <- 720 / dpi
-    height <- 480 / dpi * nrow(FIPS)
+    dpi <- 90
+    width <- 1440 / dpi
+    height <- 900 / dpi * nrow(FIPS)
     
     # Create plot
     p <- qplot(year, Total_PM2.5, data = dat, geom = geom, method = method, se = FALSE,
